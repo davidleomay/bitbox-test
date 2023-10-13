eebb63e223eef3120f4b97e9d269e3c338eed29b
David May <david.leo.may@gmail.com>
2023-10-13 09:38:46 +0200

    Added minimal React project

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import * as bitbox from "bitbox-api";
+
+function App() {
+  // Run this in e.g. a button onClick event handler.
+  async function example() {
+    try {
+      const onClose = () => {
+        // Handle disconnect.
+      };
+      const unpaired = await bitbox.bitbox02ConnectAuto(onClose);
+      const pairing = await unpaired.unlockAndPair();
+      const pairingCode = pairing.getPairingCode();
+      if (pairingCode !== undefined) {
+        // Display pairingCode to user
+      }
+      const bb02 = await pairing.waitConfirm();
+      console.log("Product", bb02.product());
+      console.log("Supports Ethereum functionality (Multi edition)?", bb02.ethSupported());
+      const deviceInfos = await bb02.deviceInfo();
+      console.log("Device infos:", deviceInfos);
+    } catch (err) {
+      const typedErr = bitbox.ensureError(err);
+      console.log(typedErr);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={example}>Connect</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
